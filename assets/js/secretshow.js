f4b241e5569ee83f144ebe6a9d95a237fc5ed7ec
Amanda Hoshor <hoshor.manda@gmail.com>
2021-04-29 21:35:48 -0600

    making links flyto

--- assets/js/secretshow.js
+++ assets/js/secretshow.js
@@ -122,6 +122,23 @@ $('#rock').click(function clearDOM (){
 
 
 //--------------map
-
+function flyToVenues(currentFeature) {
+    map.flyTo({
+      center: currentFeature.geometry.coordinates,
+      zoom: 15
+    });
+  }
+  
+  function createPopUp(currentFeature) {
+    var popUps = document.getElementsByClassName('mapboxgl-popup');
+    /** Check if there is already a popup on the map and if so, remove it */
+    if (popUps[0]) popUps[0].remove();
+  
+    var popup = new mapboxgl.Popup({ closeOnClick: false })
+      .setLngLat(currentFeature.geometry.coordinates)
+      .setHTML('<h3>Music Venues</h3>' +
+        '<h4>' + currentFeature.properties.address + '</h4>')
+      .addTo(map);
+  }
 
     
